701863aced0773ad0e24544fe13bd1499499535a
rackpack <32074731+rackpack@users.noreply.github.com>
2018-12-16 12:51:45 +0300

    Create Практика_2.java

---
+++ 2/Практика_2.java
@@ -0,0 +1,41 @@
+package author;
+
+public class Author
+{
+    private String name;
+    private String email;
+    private char gender;
+    Author()
+    {
+        gender = 'U';
+        name = "Default";
+        email = "default@default.com";
+    }
+    Author(String name, String email, char gender)
+    {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public char getGender()
+    {
+        return gender;
+    }
+    public String toString()
+    {
+        String s = name + " (" + gender + ") " + "at " + email;
+        return s;
+    }
+}
